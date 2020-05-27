d9d201ef2497bfddc3533af1aacb853c6df6c3e6
Skander Chouba <skander.chouba@gmail.com>
2020-05-27 08:55:02 +0100

    fix1

--- StarChart/Models/CelestialObject.cs
+++ StarChart/Models/CelestialObject.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ namespace StarChart.Models
     public class CelestialObject
     {
         public int Id { get; set; }
-        [required]
+        [Required]
         public string Name { get; set; }
         public int? OrbitedObjectId { get; set; }
 
